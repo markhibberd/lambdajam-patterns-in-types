75f2147d2e50dec6e041224651f46ef11cd5c25f
markhibberd <mark@hibberd.id.au>
2013-05-17 07:25:27 +1000

    challenge1 answers

--- src/main/scala/challenge1/Result.scala
+++ src/main/scala/challenge1/Result.scala
@@ -19,7 +19,11 @@ sealed trait Result[A] {
     explosion: Throwable => X,
     fail: String => X,
     ok: A => X
-  ): X = ???
+  ): X = this match {
+    case Explosion(exception) => explosion(exception)
+    case Fail(message) => fail(message)
+    case Ok(value) => ok(value)
+  }
 
   /*
    * Exercise 1.2:
@@ -33,7 +37,7 @@ sealed trait Result[A] {
    * Hint: Try using flatMap.
    */
   def map[B](f: A => B): Result[B] =
-    ???
+    flatMap(f andThen Result.ok)
 
 
   /*
@@ -47,7 +51,7 @@ sealed trait Result[A] {
    * Hint: Try using fold.
    */
   def flatMap[B](f: A => Result[B]): Result[B] =
-    ???
+    fold(Result.explosion, Result.fail, f)
 }
 
 case class Explosion[A](exception: Throwable) extends Result[A]
