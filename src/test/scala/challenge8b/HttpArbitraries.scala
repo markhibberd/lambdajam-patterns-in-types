7e95e460cca16272cb9bff13f6a7b22ee686e564
Alex Wei <awei@atlassian.com>
2013-10-04 09:08:37 +1000

    challenge8b - added spec for monad and echo.

---
+++ src/test/scala/challenge8b/HttpArbitraries.scala
@@ -0,0 +1,56 @@
+package challenge8b
+
+import challenge0._
+import challenge8b.HttpTransformerStack._
+import org.scalacheck.{Arbitrary, Gen}, Arbitrary._, Gen._
+import challenge5.ReaderT
+
+object HttpArbitraries {
+
+  implicit val methodArbitrary: Arbitrary[Method] = Arbitrary(oneOf(Options, Get, Head, Post, Put, Delete, Trace, Connect))
+
+  implicit val headerGen: Gen[(String, String)] =
+    for {
+      h <- Gen.alphaStr
+      v <- Gen.alphaStr
+    } yield h -> v
+
+  implicit val headersGen: Gen[Headers] =
+    for {
+      v <- Gen.containerOf[List, (String, String)](headerGen)
+    } yield Headers(v.toVector)
+
+  implicit val httpReadGen: Gen[HttpRead] =
+    for {
+      method <- methodArbitrary.arbitrary
+      headers <- headersGen
+      body <- Gen.alphaStr
+    } yield HttpRead(method, body, headers)
+
+  implicit val httpStateGen: Gen[HttpState] =
+    for {
+      headers <- headersGen
+    } yield HttpState(headers)
+
+  implicit val httpReadArbitrary: Arbitrary[HttpRead] = Arbitrary(httpReadGen)
+
+  implicit val httpStateArbitrary: Arbitrary[HttpState] = Arbitrary(httpStateGen)
+
+  implicit def RWSVgen[A: Arbitrary]: Arbitrary[RWSV[A]] = Arbitrary(arbitrary[ReaderT[WSV, HttpRead, A]])
+
+  implicit def httpArbitrary[A: Arbitrary]: Arbitrary[Http[A]] =
+    Arbitrary(oneOf(
+      arbitrary[A] map (a => Http.value(a)),
+      for {
+        log <- arbitrary[String]
+        s <- httpStateGen
+        a <- arbitrary[A]
+      } yield {
+        for {
+          _ <- Http.log(log)
+          _ <- Http.httpModify(_ => s)
+          r <- Http.value(a)
+        } yield r
+      }
+    ))
+}
